0afed68b75d82ee95294e8188eef4da628023f37
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-08-18 17:46:12 +0430

    POST - NationalPark

--- ParkyAPI/Models/DTOs/NationalParkDTO.cs
+++ ParkyAPI/Models/DTOs/NationalParkDTO.cs
@@ -5,7 +5,9 @@ namespace ParkyAPI.Models.DTOs;
 public class NationalParkDTO
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string State { get; set; }
     public DateTime Created { get; set; }
     public DateTime Established { get; set; }
