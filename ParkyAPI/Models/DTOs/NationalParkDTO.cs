13ba7d59d230f239a0a2ac0f34111c0d20e1fdbf
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-08-20 07:06:03 +0430

    Contact Details in XML Documentation

--- ParkyAPI/Models/DTOs/NationalParkDTO.cs
+++ ParkyAPI/Models/DTOs/NationalParkDTO.cs
@@ -6,9 +6,9 @@ public class NationalParkDTO
 {
     public int Id { get; set; }
     [Required]
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     [Required]
-    public string State { get; set; }
+    public string State { get; set; } = "";
     public DateTime Created { get; set; }
     public DateTime Established { get; set; }
 }
