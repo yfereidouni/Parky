f532c8e9247abc1e50dd54cba4c4091b3e66e148
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-08-28 23:28:13 +0430

    Upsert was developed.

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -16,14 +16,35 @@ namespace ParkyWeb.Controllers
         public async Task<IActionResult> Index()
         {
             return View(new NationalPark() { });
-            
+
             //var result = await _npRepoo.GetAllAsync(SD.NationalParkAPIPath);
             //return View(result);
         }
 
+        
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            NationalPark obj = new NationalPark();
+            if (id == null)
+            {
+                //This will be true for Insert/Create
+                return View(obj);
+            }
+
+            //Flow will come here for update
+            obj = await _npRepoo.GetAsync(SD.NationalParkAPIPath, id.GetValueOrDefault());
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+
         public async Task<IActionResult> GetAllNationalPark()
         {
-            JsonResult jR =  Json(new { data = await _npRepoo.GetAllAsync(SD.NationalParkAPIPath) });
+            JsonResult jR = Json(new { data = await _npRepoo.GetAllAsync(SD.NationalParkAPIPath) });
             return jR;
         }
     }
