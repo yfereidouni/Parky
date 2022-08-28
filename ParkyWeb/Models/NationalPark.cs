ed11bc0c5fe9630e1738c6bc4f31e89da9f4a235
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-08-28 15:29:27 +0430

    Retrieve data by AJAX calls.

--- ParkyWeb/Models/NationalPark.cs
+++ ParkyWeb/Models/NationalPark.cs
@@ -10,6 +10,6 @@ public class NationalPark
     [Required]
     public string State { get; set; } = "";
     public DateTime Created { get; set; }
-    public byte[] Picture { get; set; }
+    //public byte[] Picture { get; set; }
     public DateTime Established { get; set; }
 }
