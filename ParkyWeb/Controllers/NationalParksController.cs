996220abee467620946a21cb0825bb6ac282f92c
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-08-28 14:17:58 +0430

    Create Action method and Index View UI of ParkyWeb

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _npRepoo;
+
+        public NationalParksController(INationalParkRepository npRepoo)
+        {
+            _npRepoo = npRepoo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new NationalPark() { });
+            //return View();
+        }
+
+        public async Task<IActionResult> GetAllNationalPark()
+        {
+            return Json(new { data = await _npRepoo.GetAllAsync(SD.NationalParkAPIPath) });
+        }
+    }
+}
