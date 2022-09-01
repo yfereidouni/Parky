c52270ef4a4e1855d72d84b84c467f86b7118f1c
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-02 00:26:44 +0430

    Change Home UI

---
+++ ParkyWeb/Models/ViewModel/IndexVM.cs
@@ -0,0 +1,7 @@
+﻿namespace ParkyWeb.Models.ViewModel;
+
+public class IndexVM
+{
+    public IEnumerable<NationalPark> NationalParkList { get; set; }
+    public IEnumerable<Trail> TrailList { get; set; }
+}
