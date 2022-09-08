b446f44581b721dc49b7c024dca4d2c6fcca366b
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-09-08 20:51:21 +0430

    Registering of user was developed.

--- ParkyAPI/ConfigureSwaggerOptions.cs
+++ ParkyAPI/ConfigureSwaggerOptions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
 
@@ -21,9 +22,40 @@ public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
             options.SwaggerDoc(desc.GroupName, new Microsoft.OpenApi.Models.OpenApiInfo()
             {
                 Title = $"Parky API {desc.ApiVersion}",
-                Version=desc.ApiVersion.ToString(),
+                Version = desc.ApiVersion.ToString(),
             });
         }
+
+        // Adding Authorize Button for JWT Bearer Token Pop-up --------------------------
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+            "Enter 'Bearer' [space] and then your token in the text input below \r\n\r\n" +
+            "Example: \"Bearer 12112312cded\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+        //---------------------------------------------------------------------------------
 
         // Adding XML Documentation to UI --------------------------------------------------
         var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
